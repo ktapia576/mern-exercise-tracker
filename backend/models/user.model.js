1f5cd4aa509c6a3d3ddecfb8999546f7bc8dc573
ktapia576 <ktapia576@hotmail.com>
2020-10-18 22:56:57 -0400

    Add exercise schema

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const Schema = mongoose.Schema;
 
-const userSchema = new Schema({
+const userSchema = new Schema({ // Has one field username
   username: {
     type: String,
     required: true,
